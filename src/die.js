e9ca0be7bb93a9bccee33985d257056d8bfffc94
sriyaT <tsriya6@gmail.com>
2020-09-23 18:09:58 +0530

    initial commit

---
+++ src/die.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './die.css';
+class Die extends Component {
+  render() {
+    return (
+      <i
+        className={`Die fas fa-dice-${this.props.face} ${
+          this.props.rolling && 'shaking'
+        }`}
+      />
+    );
+  }
+}
+export default Die;
